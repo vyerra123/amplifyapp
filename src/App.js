6f11c55f06d82ef177dfa33440b52c77d4974b3e
vyerra123 <vyerra@my.bridgeport.edu>
2022-04-14 11:06:18 -0400

    latest

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>My Notes App in cloud</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
         placeholder="Note name"
